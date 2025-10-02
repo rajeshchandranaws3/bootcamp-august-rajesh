a897b323e80a0cc0eb96543b383eef73ba92205d
rajesh <rajeshchandran007@gmail.com>
2025-10-02 20:58:17 +0530

    updated

--- class8-terraform-ecs/assignment/ec2.tf
+++ class8-terraform-ecs/assignment/ec2.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "private" {
   instance_type          = var.instance_type
   subnet_id              = aws_subnet.private1.id
   availability_zone      = aws_subnet.private1.availability_zone
-  vpc_security_group_ids = [aws_security_group.public_ssh_sg.id, ]
+  vpc_security_group_ids = [aws_security_group.private_ssh_sg.id, ]
   key_name               = "my-vpc-key"
   tags = merge(
     local.common_tags,
@@ -17,7 +17,7 @@ resource "aws_instance" "public" {
   ami                    = data.aws_ami.amazon_linux.id
   instance_type          = var.instance_type
   subnet_id              = aws_subnet.public.id
-  vpc_security_group_ids = [aws_security_group.private_ssh_sg.id, ]
+  vpc_security_group_ids = [aws_security_group.public_ssh_sg.id, ]
   key_name               = "my-vpc-key"
   availability_zone      = aws_subnet.public.availability_zone
 
