58423f808f8151fcd454ef3e18c2014d4a47efbb
rajesh <rajeshchandran007@gmail.com>
2025-11-12 21:29:19 +0530

    boto3

--- class16-python/boto3-lambda.py
+++ class16-python/boto3-lambda.py
@@ -2,25 +2,25 @@ import boto3
 
 lambda_client = boto3.client('lambda')
 
-lambda_arn = 'arn:aws:lambda:ap-south-1:879381241087:function:iam-key-rotation'
-tag_key = 'team'
-
 
 ##### Function to print all lambda functions (name, runtime, and arn) #####
 
 def print_lambda_functions():
     response = lambda_client.list_functions()
-    for function in response.get('Functions', []):
+    functions = response.get('Functions', [])
+    functions.sort(key=lambda f: f.get('FunctionName', ''))
+    for function in functions:
         function_name = function.get('FunctionName')
         runtime = function.get('Runtime')
         arn = function.get('FunctionArn')
-        print(f"Lambda Function Name: {function_name}")
-        print(f"Python Version: {runtime}")
-        print(f"ARN: {arn}")
+        print(f"Lambda Name   : {function_name}")
+        print(f"Lambda Runtime: {runtime}")
+        print(f"ARN           : {arn}")
         print("----")
 
-print("---- Listing Lambda Function Details ----")
-print_lambda_functions()
+# print("---- Listing Lambda Function Details ----")
+# print("")
+# print_lambda_functions()
 
 
 ##### Function to print all lambda functions along with their tags (name, version, tags) #####
@@ -29,7 +29,9 @@ def print_lambda_functions_and_tags():
     lambda_info = []
     paginator = lambda_client.get_paginator('list_functions')
     for page in paginator.paginate():
-        for fn in page.get('Functions', []):
+        functions = page.get('Functions', [])
+        functions.sort(key=lambda f: f.get('FunctionName', ''))
+        for fn in functions:
             name = fn.get('FunctionName')
             runtime = fn.get('Runtime')
             arn = fn.get('FunctionArn')
@@ -38,8 +40,8 @@ def print_lambda_functions_and_tags():
                 tags = tags_resp.get('Tags', {}) or {}
             except Exception:
                 tags = {}
-            print(f"Lambda Name: {name}")
-            print(f"Python Version: {runtime}")
+            print(f"Lambda Name   : {name}")
+            print(f"Lambda Runtime: {runtime}")
             if tags:
                 print("Tags:")
                 for k, v in tags.items():
@@ -51,6 +53,7 @@ def print_lambda_functions_and_tags():
 
 
 # print("---- Listing Lambda Functions & Tags ----")
+# print("")
 # print_lambda_functions_and_tags()
 
 
@@ -66,7 +69,13 @@ def get_lambda_tag_value(lambda_arn, tag_key):
 
     return output
 
+
 # print("---- Getting Specific Tag Value ----")
+# print("")
+
+# lambda_arn = 'arn:aws:lambda:us-east-1:307946636515:function:lambda_1'
+# tag_key = 'owner'
+
 # print(get_lambda_tag_value(lambda_arn, tag_key))
 
 
@@ -74,19 +83,22 @@ def get_lambda_tag_value(lambda_arn, tag_key):
 
 def get_all_lambda_function_details():
     response = lambda_client.list_functions()
-    temp_lambda = []
-    for function in response['Functions']:
+    lambda_list = []
+    functions = response.get('Functions', [])
+    functions.sort(key=lambda f: f.get('FunctionName', ''))
+    for function in functions:
         function_name = function['FunctionName']
         runtime = function['Runtime']
         lambda_arn = function['FunctionArn']
-        temp_lambda.append((function_name, runtime, lambda_arn))
-    return temp_lambda
+        lambda_list.append((function_name, runtime, lambda_arn))
+    return lambda_list
 
 # print("---- Getting All Lambda Function Details ----")
+# print("")
 # print(get_all_lambda_function_details())
 
 
-##### Generate report of lambda name, python version and team tag value #####
+##### Generate report of lambda name, python version and owner tag value #####
 
 def generate_lambda_report():
     lambda_list = get_all_lambda_function_details()
@@ -95,8 +107,9 @@ def generate_lambda_report():
         # print(items)
         name, version, arn = items
         # print(name, version, arn)
-        team = get_lambda_tag_value(arn, 'team')
-        print(f"Lambda Name: {name}, Python Version: {version}, Team: {team}")
+        owner = get_lambda_tag_value(arn, 'owner')
+        print(f"Lambda Name: {name}, Lambda Version: {version}, owner: {owner}")
 
-# print("---- Generating Lambda Report ----")
-# generate_lambda_report()
+print("---- Generating Lambda Report ----")
+print("")
+generate_lambda_report()
